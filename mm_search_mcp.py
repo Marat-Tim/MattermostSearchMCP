363eb5b03d8a2e9d713725ecbc33a80027b2743f
Marat-Tim <rurikcat@gmail.com>
2026-04-29 15:59:19 +0300

    change system instructions

--- mm_search_mcp.py
+++ mm_search_mcp.py
@@ -3,9 +3,11 @@ from fastmcp import FastMCP
 mcp = FastMCP(
     "Mattermost search MCP",
     instructions=f"""
-    It's normal to make a lot of calls to retrieve related objects.
+    Company mattermost address is `https://mattermost.raiffeisen.ru/raiffeisenbank`.
+    Links to message typically looks like `https://mattermost.raiffeisen.ru/raiffeisenbank/pl/j6pwd78ge7rmuf5gfuknb8wbec`.
+    Last part of path is ID of message.
+    If user want to get message, do not send id of message. You must create link and send link. 
     !!! VERY IMPORTANT: Never give users the "id" field(or channel_id, user_id e.g)
     because they won't understand what it means.
-    You need to use other tools to find proper names based on those IDs.
     """
 )
